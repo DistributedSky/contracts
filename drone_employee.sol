ad7b655d771a84566088ed41f2321a67d190658b
simon <simonpavlov2013@gmail.com>
2016-02-23 00:15:46 +0300

    fix wrong ros types

--- drone_employee.sol
+++ drone_employee.sol
@@ -3,7 +3,7 @@ import 'atc_interface';
 contract DroneEmployee is ROSCompatible, Aircraft {
     Publisher targetPub;
     ATC controller;
-    
+
     event TakeFlight();
     event SetRoute();
 
@@ -13,25 +13,26 @@ contract DroneEmployee is ROSCompatible, Aircraft {
     function DroneEmployee(ATC _controller) {
         controller = _controller;
         targetPub = mkPublisher("target",
-                                'dron_common_msgs/RouteResponse');
+                                'small_atc_msgs/RouteResponse');
     }
-    
+
     function addCheckpoint(int256 latitude, int256 longitude, int256 altitude) {
         checkpoints[checkpoints.length++] =
             new SatFix(latitude, longitude, altitude);
     }
-    
+
     function takeFlight() {
         TakeFlight();
         controller.makeRoute(checkpoints);
     }
 
     function setRoute(RouteResponse _res) {
+
         SetRoute();
-        
+
         if (_res.valid())
             targetPub.publish(_res);
-            
+
         delete checkpoints;
     }
 }
